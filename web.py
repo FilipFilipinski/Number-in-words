a1e391441b6a53120e892173b3d5cb9d56df3180
Max Cyprys <mc.cyprysik@icloud.com>
2022-04-21 08:23:18 +0200

    flaks

--- web.py
+++ web.py
@@ -3,9 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/<value>')
+def render_result(value: str):
+    return f'{value}'
+
+
 @app.route('/')
-def home():
-    return 'Hello World'
+def render_form(value: str):
+    return f'{value}'
 
 
 if __name__ == '__main__':
