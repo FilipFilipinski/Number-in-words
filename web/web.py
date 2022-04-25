007797ee3cb0da616a183940eeea634ed08edfb1
Maximilian Cyprys <mc.cyprysik@icloud.com>
2022-04-25 18:52:31 +0200

    falsk-variable-link

--- web/web.py
+++ web/web.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template('number.html')
+
+
+@app.route('/<name>/')
+def user_view(name):
+    return name
 
 
 if __name__ == '__main__':
