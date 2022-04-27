8d75c592d8c7becd2ba1692b97d3010b3792f7fb
Max Cyprys <mc.cyprysik@icloud.com>
2022-04-27 18:59:35 +0200

    cleaning

--- web/tests/api_tests.py
+++ web/tests/api_tests.py
@@ -5,7 +5,6 @@ import json
 def tests():
     link = 'http://127.0.0.1:5000/api/'
     assert json.loads(requests.get(link + '12').content) == {'12': 'twelve'}
-    print(json.loads(requests.get(link + '12a').content))
     assert json.loads(requests.get(link + '12a').content) == {'message': 'Bad Request', 'status': 400}
     assert json.loads(requests.get(link + '10').content) == {'10': 'ten'}
     assert json.loads(requests.get(link + 'test').content) == {'message': 'Bad Request', 'status': 400}
